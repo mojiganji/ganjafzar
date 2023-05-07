410cc9bef882f3e0e5963304526e2200155f6234
mojiganji <107506754+mojiganji@users.noreply.github.com>
2023-05-07 15:29:59 +0430

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from ganjafzar import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home),
+]
