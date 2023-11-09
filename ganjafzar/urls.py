2c23118eae812ab11db675675ca42bf5939f4528
mojganganji <107506754+mojiganji@users.noreply.github.com>
2023-11-09 11:11:29 +0330

    add env

---
+++ ganjafzar/urls.py
@@ -0,0 +1,18 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from ganjafzar import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home),
+    path('programming' , views.programming),
+    path('neural-network' , views.neural),
+    path('data-maining' , views.data),
+    path('machine-learning' , views.machine),
+]
+
---
+++ ganjafzar/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+def home(request):
+    return render(request, 'home.html')
+    
+def programming(request):
+    return render(request, 'programming.html')
+
+def neural(request):
+    return render(request, 'neural.html')
+
+
+def data(request):
+    return render(request, 'data.html')
+
+
+def machine(request):
+    return render(request, 'machine.html')
