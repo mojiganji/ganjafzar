1c9e9feed265757f13096385c668ae1475ce0b17
mojganganji <107506754+mojiganji@users.noreply.github.com>
2023-11-11 10:06:36 +0330

    change home.html

--- views.py
+++ views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
     
 def programming(request):
     return render(request, 'programming.html')
