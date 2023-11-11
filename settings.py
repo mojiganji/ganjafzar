81bfe1024555ff4999546dda7d66bac3e2d96175
mojganganji <107506754+mojiganji@users.noreply.github.com>
2023-11-11 17:44:11 +0330

    Update settings.py

--- settings.py
+++ settings.py
@@ -120,12 +120,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-os.path.join(BASE_DIR,'assets'),
 
-)
+
+
+
+
+
+
+# moghe run local hidden shod
+#STATIC_URL = '/static/'
+
+#STATICFILES_DIRS = (
+#os.path.join(BASE_DIR,'assets'),
+
+#)
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
