529c8f1b5d8476c53d7c131a1aedfd175abff217
mojganganji <107506754+mojiganji@users.noreply.github.com>
2023-11-23 19:02:32 +0330

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -3,4 +3,4 @@ from accounts.forms import UserCreationForm
 # Create your views here.
 def signup_view(request):
     form=UserCreationForm()
-    return render(request , 'accounts/signup.html' , {'form':form})
+    return render(request , 'signup.html' , {'form':form})
